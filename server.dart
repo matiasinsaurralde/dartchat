3e5400c938ead0ac7a37552c0916197ca1634b95
Matias Insaurralde <matiasbaruchinsaurralde@gmail.com>
2014-02-03 04:56:19 -0300

    it's working :)

--- server.dart
+++ server.dart
@@ -1,8 +1,7 @@
-import 'dart:io';
+import 'package:redis_client/redis_client.dart';
 
-void handleRequest( message ){
-  print("new client!");
-}
+import 'dart:io';
+import 'dart:convert';
 
 void main() {
   
@@ -11,9 +10,26 @@ void main() {
   HttpServer.bind( '0.0.0.0', 5000 ).then( (server )  {
     
     server.listen( (HttpRequest req) {
+      
       WebSocketTransformer.upgrade(req).then((WebSocket websocket) {
-        print("replying with hello...");
-        websocket.add("hello!");
+
+        websocket.listen( ( rawData ) {
+          RedisClient.connect( '127.0.0.1:6379' ).then( (redis) {
+            redis.publish( 'chat', rawData );
+            redis.close();
+          });
+          
+          var data = JSON.decode(rawData); 
+        });
+        
+        RedisClient.connect( '127.0.0.1:6379').then( (redis) {
+          redis.subscribe( ['chat'], (Receiver message) {
+            message.receiveMultiBulkStrings().then((List<String> message){
+              websocket.add( message.last );
+            });
+          });
+        });
+        
       });
       
     });
--- web/chattest.dart
+++ web/chattest.dart
@@ -1,21 +1,62 @@
 import 'dart:html';
+import 'dart:convert';
+import 'package:css_animation/css_animation.dart';
 
 void main() {
+  
 /*  querySelector("#sample_text_id")
     ..text = "Click me!"
     ..onClick.listen(reverseText);*/
   var ws = new WebSocket( 'ws://127.0.0.1:5000/chat');
+  
+  var welcomeBox = querySelector('#welcome');
+  var chatBox = querySelector('#chat');
+  var messageForm = querySelector('#messageForm');
+  var nickname;
 
   ws.onMessage.listen((MessageEvent e) {
-    print('Received message: ${e.data}');
-  });
-}
+    
+    if ( chatBox.children.length > 6 ) {
+      chatBox.children.remove( chatBox.children[0] );
+    };
+    
+    var message = JSON.decode( e.data );
+    var messageHtml = '<p>';
+    
+    switch( message['type'] ) {
+      case 'event':
 
-void reverseText(MouseEvent event) {
-  var text = querySelector("#sample_text_id").text;
-  var buffer = new StringBuffer();
-  for (int i = text.length - 1; i >= 0; i--) {
-    buffer.write(text[i]);
-  }
-  querySelector("#sample_text_id").text = buffer.toString();
+        switch( message['name']) {
+          case 'newUser':
+            messageHtml += '<b>' + message['data'] + '</b> has joined the chat!';
+            break;
+        };
+        break;
+      case 'message':
+        messageHtml += '<b>' + message['name'] + ':</b> ' + message['data'];
+        break;
+    };
+    messageHtml+="</p>";
+    querySelector('#chat').appendHtml( messageHtml );
+    
+  });  
+  
+  var animation = new CssAnimation('opacity', 0, 1);
+  
+  querySelector('#nickname').focus();
+  
+  welcomeBox.querySelector('form').onSubmit.listen( ( Event e ) {
+    nickname = welcomeBox.querySelector('#nickname').value;
+    new CssAnimation( 'opacity', 1, 0).apply( welcomeBox, duration: 500, onComplete: () { welcomeBox.style.display='none'; });
+    ws.sendString( JSON.encode({ 'type': 'event', 'name': 'newUser', 'data': nickname }) );
+    messageForm.querySelector('input').focus();
+    e.preventDefault();
+  });
+  
+  
+  messageForm.onSubmit.listen( ( Event e ) {
+    ws.sendString( JSON.encode({ 'type': 'message', 'name': nickname, 'data':messageForm.querySelector('input').value  }) );
+    messageForm.querySelector('input').value = '';
+    e.preventDefault();
+  });
 }
